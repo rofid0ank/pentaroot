28e36e17cb477d053dd920963749ebc051c0b198
Rofi <102268507+rofid0ank@users.noreply.github.com>
2022-08-27 20:07:39 +0700

    Create redhawk_install.sh

---
+++ redhawk_install.sh
@@ -0,0 +1,30 @@
+:"
++-----------------------------+
+| program : install redhawk   |
+| pembuat : pentaroot         |
++-----------------------------+
+"
+
+install_xerxes()
+{
+  clear
+  echo ""
+  echo "\e[1;33m[+] \e[1;32mpentaroot"
+  echo "\e[1;33m[+] \e[1;32msaya bisa dan saya akan memperlihatkannya"
+  echo ""
+  sleep 5
+  clear
+  cd ~/
+  apt-get update -y
+  apt-get install php -y
+  apt-get install php-xml -y
+  apt-get install php-curl -y
+  apt-get install git -y
+  git clone https://github.com/Tuhinshubhra/RED_HAWK
+  clear
+  echo ""
+  echo "\e[1;33m[+] \e[1;32mredhawk berhasil di install"
+  echo ""
+}
+
+install_xerxes
